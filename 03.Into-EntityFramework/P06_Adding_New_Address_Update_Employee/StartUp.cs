a6e240284f8224034c25904d0602149fe6a2e47b
Daniel Atanasov <dakh@abv.bg>
2022-02-08 12:48:53 +0200

    P06_Adding_New_Address_Update_Employee

---
+++ 03.Into-EntityFramework/P06_Adding_New_Address_Update_Employee/StartUp.cs
@@ -0,0 +1,30 @@
+﻿
+using P02_Database_First.Data;
+using P02_Database_First.Data.Models;
+
+var dbContext = new SoftUniContext();
+
+using (dbContext)
+{
+    var address = new Address()
+    {
+        AddressText = "Vitoshka 15",
+        TownId = 4,
+        Employees = new List<Employee>()
+        {
+            dbContext.Employees.FirstOrDefault(e => e.LastName == "Nakov")
+        }
+    };
+    dbContext.Addresses.Add(address);
+
+    dbContext.SaveChanges();
+
+    Console.WriteLine(
+        String.Join(Environment.NewLine,
+        dbContext
+        .Employees
+        .OrderByDescending(e => e.AddressId)
+        .Take(10)
+        .Select(e => e.Address.AddressText)
+        ));
+}
