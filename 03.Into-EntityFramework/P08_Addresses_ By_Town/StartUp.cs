4c95479d00ef7c9d6090b75a0e40310a7eb3c358
Daniel Atanasov <dakh@abv.bg>
2022-02-08 15:18:05 +0200

    P08_Addresses_By_Town

---
+++ 03.Into-EntityFramework/P08_Addresses_ By_Town/StartUp.cs
@@ -0,0 +1,24 @@
+﻿
+using P02_Database_First.Data;
+
+var dbContext = new SoftUniContext();
+
+using (dbContext)
+{
+    var result = dbContext.Addresses
+        .OrderByDescending(a => a.Employees.Count)
+        .ThenBy(a => a.Town.Name)
+        .ThenBy(a => a.AddressText)
+        .Take(10)
+        .Select(a => new
+        {
+            AddressText = a.AddressText,
+            TownName = dbContext.Towns.FirstOrDefault(t => t.TownId == a.TownId).Name,
+            EmployeesCount = a.Employees.Count()
+        });
+
+    foreach (var address in result)
+    {
+        Console.WriteLine($"{address.AddressText}, {address.TownName} - {address.EmployeesCount} employees");
+    }
+}
